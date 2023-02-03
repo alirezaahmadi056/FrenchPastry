e2a0b142aa51cdefe69602c2f15a1031f4f10ed1
AlirezaAhmadi <ahmadi056.79@gmail.com>
2023-02-03 18:51:22 +0330

    change font family

--- app/src/main/java/info/alirezaahmadi/frenchpastry/ui/activity/LoginActivity.kt
+++ app/src/main/java/info/alirezaahmadi/frenchpastry/ui/activity/LoginActivity.kt
@@ -1,7 +1,9 @@
 package info.alirezaahmadi.frenchpastry.ui.activity
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import info.alirezaahmadi.frenchpastry.databinding.ActivityLoginBinding
 import info.alirezaahmadi.frenchpastry.mvp.model.ModelLoginActivity
 import info.alirezaahmadi.frenchpastry.mvp.presenter.PresenterLoginActivity
@@ -16,9 +18,31 @@ class LoginActivity : AppCompatActivity() {
         val view = ViewLoginActivity(this)
         setContentView(view.binding.root)
 
+        hideStatus()
+
         val model = ModelLoginActivity()
         presenter = PresenterLoginActivity(view, model)
         presenter.onCreate()
+
+    }
+
+    private fun hideStatus() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            val attrib = window.attributes
+            attrib.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+
+        } else {
+
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+
+        }
 
     }
 
