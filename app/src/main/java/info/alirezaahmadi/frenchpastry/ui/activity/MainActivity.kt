f5d1a831727bd2a679a948c8137e8c7baed42fe2
AlirezaAhmadi <ahmadi056.79@gmail.com>
2023-03-14 04:14:07 +0330

    create navigation drawer design

--- app/src/main/java/info/alirezaahmadi/frenchpastry/ui/activity/MainActivity.kt
+++ app/src/main/java/info/alirezaahmadi/frenchpastry/ui/activity/MainActivity.kt
@@ -1,12 +1,31 @@
 package info.alirezaahmadi.frenchpastry.ui.activity
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import info.alirezaahmadi.frenchpastry.R
+import info.alirezaahmadi.frenchpastry.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolBar,
+            R.string.open,
+            R.string.close
+        )
+        toggle.isDrawerIndicatorEnabled = true
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
     }
+
 }
