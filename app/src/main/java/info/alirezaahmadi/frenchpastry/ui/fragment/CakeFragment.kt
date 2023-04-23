0a10f90e9ef819ea6cad316e6a40239bfffbd121
AlirezaAhmadi <ahmadi056.79@gmail.com>
2023-04-24 00:47:29 +0430

    create cake fragment

---
+++ app/src/main/java/info/alirezaahmadi/frenchpastry/ui/fragment/CakeFragment.kt
@@ -0,0 +1,30 @@
+package info.alirezaahmadi.frenchpastry.ui.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import info.alirezaahmadi.frenchpastry.mvp.model.ModelPastryCatsFragment
+import info.alirezaahmadi.frenchpastry.mvp.presenter.PresenterPastryCatsFragment
+import info.alirezaahmadi.frenchpastry.mvp.view.ViewPastryCatsFragment
+
+class CakeFragment(
+    private val mContext: Context
+) : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val view = ViewPastryCatsFragment(mContext)
+        val presenter = PresenterPastryCatsFragment(view, ModelPastryCatsFragment(), mContext)
+        presenter.onCreate()
+        return view.binding.root
+
+    }
+
+}
