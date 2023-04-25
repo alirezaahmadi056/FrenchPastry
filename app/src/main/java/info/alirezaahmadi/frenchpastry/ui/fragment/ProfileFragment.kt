212812713e3fdae4c935e5cb2aa3b8fb9d77b00e
AlirezaAhmadi <ahmadi056.79@gmail.com>
2023-04-26 00:11:59 +0430

    create profile fragment

---
+++ app/src/main/java/info/alirezaahmadi/frenchpastry/ui/fragment/ProfileFragment.kt
@@ -0,0 +1,30 @@
+package info.alirezaahmadi.frenchpastry.ui.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import info.alirezaahmadi.frenchpastry.mvp.model.ModelCakeCatsFragment
+import info.alirezaahmadi.frenchpastry.mvp.presenter.PresenterCakeCatsFragment
+import info.alirezaahmadi.frenchpastry.mvp.view.ViewCakeCatsFragment
+
+class ProfileFragment(
+    private val mContext: Context
+) : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val view = ViewCakeCatsFragment(mContext)
+        val presenter = PresenterCakeCatsFragment(view, ModelCakeCatsFragment(), mContext)
+        presenter.onCreate()
+        return view.binding.root
+
+    }
+
+}
